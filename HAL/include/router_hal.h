b885cbdf7d841b4033c333997572383baa75fbee
Jiajie Chen <noc@jiegec.ac.cn>
2019-08-23 16:01:01 +0800

    Improve HAL header

--- HAL/include/router_hal.h
+++ HAL/include/router_hal.h
@@ -1,8 +1,8 @@
 #ifndef __ROUTER_HAL_H__
 #define __ROUTER_HAL_H__
 
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdlib.h>
 #ifdef ROUTER_BACKEND_LINUX
 #include <arpa/inet.h>
 #elif defined ROUTER_BACKEND_MACOS
@@ -29,9 +29,10 @@ extern "C" {
 
 /**
  * @brief 初始化，在所有其他函数调用前调用且仅调用一次
- * 
+ *
  * @param debug IN，零表示关闭调试信息，非零表示输出调试信息到标准错误输出
- * @param if_addrs IN，包含 N_IFACE_ON_BOARD 个 IPv4 地址，对应每个端口的 IPv4 地址
+ * @param if_addrs IN，包含 N_IFACE_ON_BOARD 个 IPv4 地址，对应每个端口的 IPv4
+ * 地址
  *
  * @return int 0 表示成功，非 0 表示失败
  */
@@ -47,7 +48,9 @@ uint64_t HAL_GetTicks();
 /**
  * @brief 从 ARP 表中查询 IPv4 对应的 MAC 地址
  *
- * 如果是表中不存在的IP，系统将自动发送 ARP 报文进行查询，待对方主机回应后可重新调用本接口从表中查询
+ * 如果是表中不存在的IP，系统将自动发送 ARP
+ * 报文进行查询，待对方主机回应后可重新调用本接口从表中查询 部分后端会限制发送的
+ * ARP 报文数量，如每秒向同一个主机最多发送一个 ARP 报文
  *
  * @param if_index IN，接口索引号，[0, N_IFACE_ON_BOARD-1]
  * @param ip IN，要查询的 IP 地址
@@ -57,7 +60,7 @@ uint64_t HAL_GetTicks();
 int HAL_ArpGetMacAddress(int if_index, in_addr_t ip, macaddr_t o_mac);
 
 /**
- * @brief 获取网卡的 MAC 地址
+ * @brief 获取网卡的 MAC 地址，如果为全 0 代表系统中不存在该网卡或者获取失败
  *
  * @param if_index IN，接口索引号，[0, N_IFACE_ON_BOARD-1]
  * @param o_mac OUT，网卡的 MAC 地址
@@ -66,9 +69,13 @@ int HAL_ArpGetMacAddress(int if_index, in_addr_t ip, macaddr_t o_mac);
 int HAL_GetInterfaceMacAddress(int if_index, macaddr_t o_mac);
 
 /**
- * @brief 接收一个 IPv4 报文，保证不会收到自己发送的报文
+ * @brief 接收一个 IPv4
+ * 报文，保证不会收到自己发送的报文；请保证缓冲区大小足够大（如大于常见的
+ * MTU），报文只能读取一次
  *
- * @param if_index_mask IN，接口索引号的 bitset，最低的 N_IFACE_ON_BOARD 位有效，对于每一位，1 代表接收对应接口，0 代表不接收；部分平台要求所有接口都开启接收
+ * @param if_index_mask IN，接口索引号的 bitset，最低的 N_IFACE_ON_BOARD
+ * 位有效，对于每一位，1 代表接收对应接口，0
+ * 代表不接收；部分平台仅支持所有接口都开启接收的情况
  * @param buffer IN，接收缓冲区，由调用者分配
  * @param length IN，接收缓存区大小
  * @param src_mac OUT，IPv4 报文下层的源 MAC 地址
@@ -78,7 +85,8 @@ int HAL_GetInterfaceMacAddress(int if_index, macaddr_t o_mac);
  * @return int >0 表示实际接收的报文长度，=0 表示超时返回，<0 表示发生错误
  */
 int HAL_ReceiveIPPacket(int if_index_mask, uint8_t *buffer, size_t length,
-                        macaddr_t src_mac, macaddr_t dst_mac, int64_t timeout, int *if_index);
+                        macaddr_t src_mac, macaddr_t dst_mac, int64_t timeout,
+                        int *if_index);
 
 /**
  * @brief 发送一个 IP 报文，它的源 MAC 地址就是对应接口的 MAC 地址
