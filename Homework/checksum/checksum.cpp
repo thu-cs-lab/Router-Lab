f1a79655dc41a6a3c90b913243c77db3ce3f5c5d
Jiajie Chen <noc@jiegec.ac.cn>
2019-08-26 00:54:56 +0800

    Add checksum homework boilerplate

---
+++ Homework/checksum/checksum.cpp
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+/**
+ * @brief 进行 IP 头的校验和的验证，无误则返回 true ，有误则返回 false
+ */
+bool validateIPChecksum(uint8_t *packet, size_t len) {
+  // TODO
+  return true;
+}
---
+++ Homework/checksum/main.cpp
@@ -0,0 +1,31 @@
+#include "router_hal.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+extern bool validateIPChecksum(uint8_t *packet, size_t len);
+
+in_addr_t addrs[N_IFACE_ON_BOARD] = {0};
+uint8_t packet[1024];
+
+int main(int argc, char *argv[]) {
+  int res = HAL_Init(0, addrs);
+  if (res < 0) {
+    return res;
+  }
+  while (1) {
+    int mask = (1 << N_IFACE_ON_BOARD) - 1;
+    macaddr_t src_mac;
+    macaddr_t dst_mac;
+    int if_index;
+    res = HAL_ReceiveIPPacket(mask, packet, sizeof(packet), src_mac,
+                                  dst_mac, -1, &if_index);
+    if (res == HAL_ERR_EOF) {
+      break;
+    } else if (res < 0) {
+      return res;
+    }
+    printf("%s\n", validateIPChecksum(packet, res) ? "Yes" : "No");
+  }
+  return 0;
+}
