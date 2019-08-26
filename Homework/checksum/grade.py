d29dad4f63962da2e95388c71b9cd96a03ae8dde
Jiajie Chen <noc@jiegec.ac.cn>
2019-08-26 20:17:13 +0800

    Improve grader to use pyshark

--- Homework/checksum/grade.py
+++ Homework/checksum/grade.py
@@ -14,6 +14,12 @@ import signal
 import glob
 import traceback
 
+try:
+    import pyshark
+except Exception:
+    print('Install pyshark (pip3 install pyshark) first!')
+    sys.exit(1)
+
 def write_grade(grade, total):
     data = {}
     data['grade'] = grade
@@ -60,7 +66,19 @@ if __name__ == '__main__':
             if out == ans:
                 grade += 1
             elif os.isatty(1):
-                os.system('diff -u {} {} | head -n 20'.format(out_file, ans_file))
+                limit = 3
+                count = 0
+                reader = pyshark.FileCapture(in_file)
+                packets = list(reader)
+                print('Wrong Answer:')
+                for i in range(len(ans)):
+                    if i >= len(out) or out[i] != ans[i]:
+                        limit += 1
+                        print('Answer is wrong for packet #{}: {}'.format(i, packets[i]['ip']))
+                        if count == limit:
+                            break
+                print('Diff: ')
+                os.system('diff -u {} {} | head -n 10'.format(out_file, ans_file))
         except Exception:
             if os.isatty(1):
                 print('Unexpected exception caught:')
