3bbfd9f586a177366a16f8210ad6443ac89815d3
Nano <nanoapezlk@gmail.com>
2019-10-29 14:05:04 +0800

    Fix minor typo in checksum

--- Homework/checksum/checksum.cpp
+++ Homework/checksum/checksum.cpp
@@ -4,7 +4,7 @@
 /**
  * @brief 进行 IP 头的校验和的验证
  * @param packet 完整的 IP 头和载荷
- * @param len 即 packet 是长度，单位是字节，保证包含完整的 IP 头
+ * @param len 即 packet 的长度，单位是字节，保证包含完整的 IP 头
  * @return 校验和无误则返回 true ，有误则返回 false
  */
 bool validateIPChecksum(uint8_t *packet, size_t len) {
