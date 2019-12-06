6d4f0f1aeebdc7cb0dee76d3b22ae9720d85787d
Jiajie Chen <noc@jiegec.ac.cn>
2019-12-06 23:56:38 +0800

    Add FAQ for wireshark, add commons to struct RTE

--- Homework/lookup/router.h
+++ Homework/lookup/router.h
@@ -1,7 +1,10 @@
 #include <stdint.h>
+
+// 路由表的一项
 typedef struct {
-    uint32_t addr;
-    uint32_t len;
-    uint32_t if_index;
-    uint32_t nexthop;
+    uint32_t addr; // 地址
+    uint32_t len; // 前缀长度
+    uint32_t if_index; // 出端口编号
+    uint32_t nexthop; // 下一条的地址，0 表示直连
+    // 为了实现 RIP 协议，需要在这里添加额外的字段
 } RoutingTableEntry;
