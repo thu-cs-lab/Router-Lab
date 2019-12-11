e8fd1be3f41b26cd4568d14e87123cec10e219ba
Jiajie Chen <noc@jiegec.ac.cn>
2019-12-11 12:08:49 +0800

    Fix typo

--- setup-r3.sh
+++ setup-r3.sh
@@ -6,7 +6,7 @@ ip netns add PC2
 ip l add veth-r3 type veth peer name veth-pc2
 ip l set veth-pc2 netns PC2
 ip a add 192.168.5.2/24 dev veth-r3
-ip netns exec PC2 ip a add 192.168.5.1/24 dev veth-pc1
+ip netns exec PC2 ip a add 192.168.5.1/24 dev veth-pc2
 ip netns exec PC2 ip l set veth-pc2 up
 ip netns exec PC2 ip r add default via 192.168.5.2
 ip r del 192.168.5.0/24 dev veth-r3
