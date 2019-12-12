569fbceb2cfc0609b030554efde4fe922844477c
Jiajie Chen <noc@jiegec.ac.cn>
2019-12-12 21:18:48 +0800

    Clarify tcp test and remove links in restore.sh

--- Setup/restore.sh
+++ Setup/restore.sh
@@ -7,3 +7,8 @@ systemctl disable bird
 dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cp $dir/dhcpcd.conf /etc/dhcpcd.conf
 systemctl restart dhcpcd
+
+ip l del netns PC1
+ip l del netns PC2
+ip l del veth-r1
+ip l del veth-r3
