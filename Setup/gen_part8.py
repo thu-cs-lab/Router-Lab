6cea063b18a2d219fe32110e804974844baee218
Jiajie Chen <noc@jiegec.ac.cn>
2019-12-21 11:15:59 +0800

    Add configs for stage 3

---
+++ Setup/gen_part8.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+with open('conf-part8.conf', 'w') as f:
+    for i in range(1, 9):
+        for j in range(0, 256):
+            f.write('route 10.%d.%d.0/24 via "wlan0";\n' % (i, j))
+
