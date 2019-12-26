a866ae5b247402c087a66bd00fc43804f2f75cde
Jiajie Chen <noc@jiegec.ac.cn>
2019-12-26 10:26:55 +0800

    Copy conf-part* to /etc/bird

--- Setup/setup-pc1.sh
+++ Setup/setup-pc1.sh
@@ -6,6 +6,9 @@ set -v
 # Setup dhcpcd config
 dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cp $dir/dhcpcd-pc1.conf /etc/dhcpcd.conf
+cp $dir/conf-part7.conf /etc/bird/
+cp $dir/conf-part8.conf /etc/bird/
+cp $dir/conf-part9.conf /etc/bird/
 cp $dir/bird1.conf /etc/bird/bird.conf
 systemctl disable bird
 systemctl stop bird
