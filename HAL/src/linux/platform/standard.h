8c4db323b811fc636584a319c80fad028893e941
Router Lab <root@routerlab.local>
2020-09-05 22:36:40 +0800

    Add static to interfaces

--- HAL/src/linux/platform/standard.h
+++ HAL/src/linux/platform/standard.h
@@ -2,7 +2,8 @@
 
 // for online experiment, don't change
 #ifdef ROUTER_R1
-const char *interfaces[N_IFACE_ON_BOARD] = {
+
+static const char *interfaces[N_IFACE_ON_BOARD] = {
     "r1pc1",
     "r1r2",
     "eth3",
@@ -11,7 +12,7 @@ const char *interfaces[N_IFACE_ON_BOARD] = {
 
 #elif defined(ROUTER_R2)
 
-const char *interfaces[N_IFACE_ON_BOARD] = {
+static const char *interfaces[N_IFACE_ON_BOARD] = {
     "r2r1",
     "r2r3",
     "eth3",
@@ -19,7 +20,8 @@ const char *interfaces[N_IFACE_ON_BOARD] = {
 };
 
 #elif defined(ROUTER_R3)
-const char *interfaces[N_IFACE_ON_BOARD] = {
+
+static const char *interfaces[N_IFACE_ON_BOARD] = {
     "r3r2",
     "r3pc2",
     "eth3",
@@ -29,7 +31,7 @@ const char *interfaces[N_IFACE_ON_BOARD] = {
 #else
 // you can customize this
 // configure this to match the output of `ip a`
-const char *interfaces[N_IFACE_ON_BOARD] = {
+static const char *interfaces[N_IFACE_ON_BOARD] = {
     "eth1",
     "eth2",
     "eth3",
