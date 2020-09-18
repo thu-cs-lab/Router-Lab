24494c2df1573a46bbcdde00fd69fe9cd5eed75e
Jiajie Chen <c@jia.je>
2020-09-18 13:29:46 +0800

    Re-arrange comments in lookup problem

--- Homework/lookup/lookup.cpp
+++ Homework/lookup/lookup.cpp
@@ -2,22 +2,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-/*
-  RoutingTable Entry 的定义如下：
-  typedef struct {
-    uint32_t addr; // 大端序，IPv4 地址
-    uint32_t len; // 小端序，前缀长度
-    uint32_t if_index; // 小端序，出端口编号
-    uint32_t nexthop; // 大端序，下一跳的 IPv4 地址
-  } RoutingTableEntry;
-
-  约定 addr 和 nexthop 以 **大端序** 存储。
-  这意味着 1.2.3.4 在小端序的机器里对应 0x04030201 而不是 0x01020304。
-  保证 addr 和 len 构成合法的网络前缀。
-  当 nexthop 为零时这是一条直连路由。
-  你可以在全局变量中把路由表以一定的数据结构格式保存下来。
-*/
-
 /**
  * @brief 插入/删除一条路由表表项
  * @param insert 如果要插入则为 true ，要删除则为 false
@@ -32,7 +16,7 @@ void update(bool insert, RoutingTableEntry entry) {
 
 /**
  * @brief 进行一次路由表的查询，按照最长前缀匹配原则
- * @param addr 需要查询的目标地址，大端序
+ * @param addr 需要查询的目标地址，网络字节序
  * @param nexthop 如果查询到目标，把表项的 nexthop 写入
  * @param if_index 如果查询到目标，把表项的 if_index 写入
  * @return 查到则返回 true ，没查到则返回 false
--- Homework/lookup/router.h
+++ Homework/lookup/router.h
@@ -1,10 +1,17 @@
 #include <stdint.h>
 
-// 路由表的一项
+/*
+  表示路由表的一项。
+  约定 addr 和 nexthop 以 **网络字节序（大端序）** 存储。
+  这意味着 1.2.3.4 在小端序的机器里对应 0x04030201 而不是 0x01020304。
+  保证 addr 和 len 构成合法的网络前缀。
+  当 nexthop 为零时这是一条直连路由。
+  你可以在全局变量中把路由表以一定的数据结构格式保存下来。
+*/
 typedef struct {
-  uint32_t addr;     // 大端序，IPv4 地址
-  uint32_t len;      // 小端序，前缀长度
-  uint32_t if_index; // 小端序，出端口编号
-  uint32_t nexthop;  // 大端序，下一跳的 IPv4 地址
+  uint32_t addr;     // 网络字节序，IPv4 地址
+  uint32_t len;      // 主机字节序，前缀长度
+  uint32_t if_index; // 主机字节序，出端口编号
+  uint32_t nexthop;  // 网络字节序，下一跳的 IPv4 地址
   // 为了实现 RIP 协议，需要在这里添加额外的字段
 } RoutingTableEntry;
