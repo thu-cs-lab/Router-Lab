591e9249560ea3ef3d90f5ed6c8416a2ab2f7f91
Router Lab <root@routerlab.local>
2021-09-03 22:56:28 +0800

    Update part9 to 11 config

--- Setup/part9to11/gen.py
+++ Setup/part9to11/gen.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
+import json
 
 with open('conf-part9.conf', 'w') as f:
     f.write('# assuming lo exists and is up\n')
     for i in range(10, 256):
-        f.write('route 192.168.%d.0/24 via "lo";\n' % (i))
+        f.write('route fd00::%x:0/112 via "lo";\n' % (i))
 
 
 with open('conf-part10.conf', 'w') as f:
     f.write('# assuming lo exists and is up\n')
     for i in range(1, 9):
         for j in range(0, 256):
-            f.write('route 10.%d.%d.0/24 via "lo";\n' % (i, j))
+            f.write('route fd00::%x:%x:0/112 via "lo";\n' % (i, j))
 
+
+# https://api.bgpview.io/asn/9808/prefixes
+data = json.load(open('as9808_prefixes'))['data']['ipv6_prefixes']
+with open('conf-part11.conf', 'w') as f:
+    f.write('# assuming lo exists and is up\n')
+    for entry in data:
+        f.write('route %s via "lo";\n' % entry['prefix'])
