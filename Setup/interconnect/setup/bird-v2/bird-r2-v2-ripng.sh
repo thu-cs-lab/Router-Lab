8057b1914baa4a3f86486c498c23331369b577d8
Jiajie Chen <c@jia.je>
2024-01-07 11:16:08 +0800

    Fix bird v2 ripng scripts

--- Setup/interconnect/setup/bird-v2/bird-r2-v2-ripng.sh
+++ Setup/interconnect/setup/bird-v2/bird-r2-v2-ripng.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Run standard RIPng on r2
-# For BIRD v2.x
+# For BIRD v2
 
 ip netns exec R2 sysctl net.ipv6.conf.r2r1.disable_ipv6=0
+ip netns exec R2 sysctl net.ipv6.conf.r2r1.accept_ra=0
 ip netns exec R2 ip a add fd00::3:2/112 dev r2r1
+ip netns exec R2 ip r add fd00::1:0/112 via fd00::3:1 dev r2r1
 ip netns exec R2 sysctl net.ipv6.conf.r2r3.disable_ipv6=0
 ip netns exec R2 ip a add fd00::4:1/112 dev r2r3
+
+# enable IPv6 forwarding
+ip netns exec R2 sysctl -w net.ipv6.conf.all.forwarding=1
 ip netns exec R2 bird -c bird-r2-v2-ripng.conf -d -s bird-r2-ripng.ctl
--- Setup/interconnect/setup/bird-v2/bird-r3-v2-ripng.sh
+++ Setup/interconnect/setup/bird-v2/bird-r3-v2-ripng.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# Run standard RIPng on r3
+# For BIRD v2
 
+ip netns exec R3 sysctl net.ipv6.conf.r3r2.disable_ipv6=0
+ip netns exec R3 ip a add fd00::4:2/112 dev r3r2
+ip netns exec R3 sysctl net.ipv6.conf.r3pc2.disable_ipv6=0
+ip netns exec R3 ip a add fd00::5:2/112 dev r3pc2
+
+# enable IPv6 forwarding
+ip netns exec R3 sysctl -w net.ipv6.conf.all.forwarding=1
 ip netns exec R3 bird -c bird-r3-v2-ripng.conf -d -s bird-r3-ripng.ctl
